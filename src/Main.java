972cc4de3f4b351c24d01e122d4ba7f929dbcfd2
Yurii <shadowjura82@gmail.com>
2023-04-03 17:06:38 +0200

    task3 has been completed

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,30 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
         System.out.println("Задание 1");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+            System.out.print(" " + i);
         }
-        System.out.println("");
+        System.out.println();
     }
 
-    public static void task2(){
+    public static void task2() {
         System.out.println("Задание 2");
         for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+            System.out.print(" " + i);
         }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
     }
 }
