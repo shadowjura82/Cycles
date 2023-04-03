8e9d7ccef0d4403ed990f01908f4329dc471db8b
Yurii <shadowjura82@gmail.com>
2023-04-03 17:16:08 +0200

    task5 has been completed

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -34,6 +35,14 @@ public class Main {
         System.out.println("Задание 4");
         for (int i = 10; i >= -10; i--) {
             System.out.print(" " + i);
+        }
+        System.out.println();
+    }
+
+    public static void task5() {
+        System.out.println("Задание 5");
+        for (int i = 1904; i < 2096; i += 4) {
+            System.out.println(i + " год является високосным");
         }
         System.out.println();
     }
