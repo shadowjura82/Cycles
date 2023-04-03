143dc061d7c0a7e1e624b1b3752b0be5ace58633
Yurii <shadowjura82@gmail.com>
2023-04-03 16:54:46 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        task1();
+    }
 
+    public static void task1() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
